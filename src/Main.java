92cca93f37276446e1646f4f90ac60e7ff5b2445
julianehiem <jules.ehiem@gmail.com>
2022-09-05 19:20:26 -0400

    creating first todo for application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+//        TODO: Create an application checklist for mvp features and dev process
     }
 }
